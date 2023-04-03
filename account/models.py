6d165b7e1bc1a5fb2272d3cfa07fb08162d1ea34
farzinsh7 <farzinnater@gmail.com>
2023-04-03 23:25:37 +0430

    Start_IDEBAZ_project

---
+++ account/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+# from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    # is_author = models.BoolianField(default=False)
+    # special_user = models.DateTimeField(default=timezone.now)
+
+
