3713ed8a3e991f043bd7b03ce91c04090c9c8c92
farzinsh7 <farzinnater@gmail.com>
2023-04-17 22:45:46 +0430

    Register HomeGallery and Information in admins.py

--- home_gallery/admin.py
+++ home_gallery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from home_gallery.models import HomeGallery, Information
+
 
 # Register your models here.
+admin.site.register(HomeGallery)
+admin.site.register(Information)
