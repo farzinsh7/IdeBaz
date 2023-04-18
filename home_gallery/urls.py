b8d944abab2606d43ae3665be7e67fdcc12906b5
farzinsh7 <farzinnater@gmail.com>
2023-04-18 22:56:41 +0430

    Create header view

--- home_gallery/urls.py
+++ home_gallery/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from home_gallery.views import home_iformation
+from home_gallery.views import home_iformation, InformationListView
 
 app_name = 'homepage'
 
 urlpatterns = [
     path('', home_iformation, name='information'),
+    path('header/', InformationListView.as_view(), name='header'),
 ]
--- home_gallery/views.py
+++ home_gallery/views.py
@@ -5,18 +5,17 @@ from .models import HomeGallery, Information
 
 # Create your views here.
 
-# class InformationListView(ListView):
-#     model = Information
-#     queryset = Information.objects.first()
-#     context_object_name = "info"
-#     template_name = "home.html"
+class InformationListView(ListView):
+    model = Information
+    queryset = Information.objects.first()
+    context_object_name = "info"
+    template_name = "base/shared/header.html"
 
 
 def home_iformation(request):
-    information = Information.objects.first()
     gallery = HomeGallery.objects.all()
     context = {
-        'info': information,
         'gallery': gallery,
     }
     return render(request, 'home.html', context)
+
