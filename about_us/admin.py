2d53a99417e48d96bc22f4d2f882ec27963f172d
farzinsh7 <farzinnater@gmail.com>
2023-04-23 15:31:36 +0430

    add About Models to admin dashboard

--- about_us/admin.py
+++ about_us/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AboutUs, Services, OurSkills
 
 # Register your models here.
+admin.site.register(AboutUs)
+admin.site.register(Services)
+admin.site.register(OurSkills)
