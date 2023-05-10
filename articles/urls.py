86d6192f81d1eebe40d50b804ba05964aa33cd7a
Farzin Shams <f.shams@ehsan-group.com>
2023-05-10 13:39:35 +0330

    Update tags link

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import  ArticleListView, ArticleDetailView
+from .views import  ArticleListView, ArticleDetailView, TagList
 
 app_name = 'article'
 
 urlpatterns = [
     path('article/', ArticleListView.as_view(), name='list_view'),
     path('article/<slug:slug>', ArticleDetailView.as_view(), name='detail_view'),
-    # path('tag/<slug:slug>', TagList.as_view(), name='tag_view'),
+    path('tags/<slug:slug>', TagList.as_view(), name='tag_view'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from . models import Article, Category, Tags
 from datetime import datetime, timedelta
 
 # Create your views here.
+
+
 class ArticleListView(ListView):
     queryset = Article.objects.published()
     model = Article
@@ -21,22 +23,17 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tags'] = Tags.objects.filter(status=True)
-        context['category'] = Category.objects.filter(status=True) 
+        context['category'] = Category.objects.filter(status=True)
         return context
 
 
-# class TagList(ListView):
-#     model = Tags
-#     paginate_by = 6
-#     template_name = 'tags.html'
+class TagList(ListView):
+    model = Tags
+    paginate_by = 6
+    template_name = 'tags.html'
 
-#     def get_queryset(self):
-#         global tag
-#         slug = self.kwargs.get('slug')
-#         tag = Tags.objects.filter(slug=slug)
-#         return tag.articles.published()
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['tag'] = tag
-#         return context
+    def get_queryset(self):
+        global tag
+        slug = self.kwargs.get('slug')
+        tag = get_object_or_404(Tags.objects.all(), slug=slug)
+        return tag.articles.published()
