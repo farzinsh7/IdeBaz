061ac2c015f22e1d927ca5716336251708db93d0
farzinsh7 <farzinnater@gmail.com>
2023-05-12 14:45:17 +0430

    create project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=200, unique=True)
+    status = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Publish')
+    )
+    client = models.CharField(max_length=150, default='user')
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=200, unique=True)
+    category = models.ManyToManyField(Category, related_name='projects')
+    description = RichTextUploadingField()
+    image = models.ImageField(upload_to='project')
+    website = models.CharField(max_length=150, null=True)
+    date = models.DateField(null=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    keywords = models.CharField(max_length=300, null=True)
+    seo_description = models.TextField(null=True)
+
+
+    def __str__(self):
+        return self.title
